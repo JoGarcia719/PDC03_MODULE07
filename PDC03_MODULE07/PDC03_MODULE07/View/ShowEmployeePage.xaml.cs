c61f15b3eed52ef7d31e644e6dc7feea33f0c1ba
JoGarcia719 <garcia.josephandrew@auf.edu.ph>
2023-06-02 12:34:52 +0800

    Add project files.

---
+++ PDC03_MODULE07/PDC03_MODULE07/View/ShowEmployeePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PDC03_MODULE07.Model;
+using PDC03_MODULE07.ViewModel;
+using System.Runtime.CompilerServices;
+
+namespace PDC03_MODULE07.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowEmployeePage : ContentPage
+    {
+        EmployeeViewModel viewModel;
+        public ShowEmployeePage()
+        {
+            InitializeComponent();
+            viewModel = new EmployeeViewModel();
+        }
+
+        private void showEmployeePage()
+        {
+            var res = viewModel.GetAllEmployees().Result;
+            lstData.ItemsSource = res;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            showEmployeePage();
+        }
+
+        private void btnAddRecord_Clicked(object sender, EventArgs e)
+        {
+            //this.Navigation.PushAsync(new AddEmployee());
+        }
+    }
+}
